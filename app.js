d61429d0c8e48abbd762d046e7102c949e232474
gary909 <garywhite61@hotmail.com>
2023-05-04 11:06:39 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 function stray(numbers) {
-
+    numbers.sort();
+    if( numbers[0] == numbers[1] ){
+        return numbers[numbers.length - 1];
+    } else {
+        return numbers[0];
+    }
 }
 
 console.log(stray([1, 1, 2])); // 2
+console.log(stray([2, 1, 1])); // 2
